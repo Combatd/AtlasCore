20fefcb1c35af9f043dbfb237d771ee2962e28f0
Combatd <combatdstar@gmail.com>
2019-12-04 21:39:56 -0800

    create basic version of User model

---
+++ models/users.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema( {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean }
+} );
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
