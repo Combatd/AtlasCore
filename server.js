15cf629f7e0387c05d960186389b2da8fb434157
Combatd <combatdstar@gmail.com>
2019-12-05 10:05:20 -0800

    added placeholder comments in server.js

--- server.js
+++ server.js
@@ -3,3 +3,15 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+require('./config/db');
+
+// controllers
+// const userController = require('./controllers/users');
+
+
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.json());
+
+
+// URL prefix
+// app.use('/auth', userController);
