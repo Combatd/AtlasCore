cf25e954c080bd98b98efccc58ef30e13f4b43f1
Combatd <combatdstar@gmail.com>
2019-12-06 14:02:41 -0800

    create .env.development file, create methods in TicketsContainer to fetch tickets and other things

---
+++ frontend/src/components/TicketsContainer/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import TicketBox from '../TicketBox';
+
+class Ticket extends React.Component {
+    constructor(props) { 
+        super(props);
+
+        this.state = {
+            tickets: []
+        }
+    }
+
+        componentWillMount() {
+            this.props.getTickets()
+        }
+
+        getTickets = async () => {
+            try {
+                const tickets = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/tickets/`, {
+                    credentials: 'include',
+                    method: 'Get',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+
+                })
+
+                const ticketsJson = await tickets.json();
+                this.setState({
+                    tickets: ticketsJson.data
+                });
+
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        deleteTicket = async (id) => {
+            const deleteTicketResponse = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/tickets/${id}`, {
+                method: 'DELETE',
+                credentials: 'include'
+            });
+
+            const deleteTicketParsed = deleteTicketResponse.json();
+            // after removing from db, remove ticket from state
+            this.setState({
+                tickets: this.state.tickets.filter( (ticket) => ticket.id !== id)
+            });
+
+        }
+
+
+
+
+        render() {
+            if (this.state.tickets.length === 0) {
+                return(
+                    <div>
+                        There are currently no tickets!
+                    </div>
+                )
+            } else {
+                return (
+                    <div>
+                    <h2>All Tickets</h2>
+                    {
+                        this.state.tickets.map(tweet => (
+                            <TicketBox key={tweet._id} text={tweet.text} />
+                        ))
+                    }
+                    </div>
+                );
+             }
+        }
+
+}
+
+export default withRouter(Ticket);
