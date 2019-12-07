0a070835eddcbb40e094047d807aa0847911c84d
Combatd <combatdstar@gmail.com>
2019-12-06 20:01:31 -0800

    add container div to Navbar

--- frontend/src/components/layout/Navbar/index.js
+++ frontend/src/components/layout/Navbar/index.js
@@ -4,20 +4,22 @@ class Navbar extends Component {
 
     render() {
         return (
-            <div>
+            <React.Fragment>
 
                 <nav>
                     <div class="nav-wrapper blue" >
-                        <a href="#" class="brand-logo">Logo</a>
-                        <ul id="nav-mobile" class="right hide-on-med-and-down">
-                            <li><a href="sass.html">Sass</a></li>
-                            <li><a href="badges.html">Components</a></li>
-                            <li><a href="collapsible.html">JavaScript</a></li>
-                        </ul>
+                        <div className="container">
+                            <a href="#" class="brand-logo">Logo</a>
+                            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                                <li><a href="sass.html">Sass</a></li>
+                                <li><a href="badges.html">Components</a></li>
+                                <li><a href="collapsible.html">JavaScript</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
 
-            </div>
+            </React.Fragment>
         )
     }
 
