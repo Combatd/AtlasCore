2170ddc5994bfd5f47c35a72aab592a6b5bef204
Combatd <combatdstar@gmail.com>
2019-12-07 15:02:11 -0800

    put state in AddModal component

---
+++ frontend/src/components/layout/AddModal/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+// import { Modal } from ../Modal
+
+class AddModal extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            title: '',
+            category: '',
+            text: ''
+        }
+
+    }
+
+}
+
+export default AddModal;
