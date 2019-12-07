fdaa4be7cfde297903385a5538a3efc074479120
Combatd <combatdstar@gmail.com>
2019-12-06 20:35:00 -0800

    modify Navbar component return method

--- frontend/src/components/layout/Navbar/index.js
+++ frontend/src/components/layout/Navbar/index.js
@@ -8,6 +8,7 @@ class Navbar extends Component {
 
                 <nav>
                     <div class="nav-wrapper blue" >
+                        <a href="#" data-target="slide-out" class="sidenav-trigger show-on-large"><i class="material-icons">menu</i></a>
                         <div className="container">
                             <a href="#" class="brand-logo">Logo</a>
                             <ul id="nav-mobile" class="right hide-on-med-and-down">
@@ -19,7 +20,21 @@ class Navbar extends Component {
                     </div>
                 </nav>
 
-            </React.Fragment>
+                <ul id="slide-out" class="sidenav">
+                    <li>
+                    <div class="user-view">
+                        <div class="background">
+                            <img src="images/office.jpg" />
+                        </div>
+                            <a href="#user"><img class="circle" src="images/yuna.jpg" /></a>
+                            <a href="#name"><span class="white-text name">John Doe</span></a>
+                            <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
+                    </div>
+                    </li>
+
+                </ul>
+                
+            </ React.Fragment>
         )
     }
 
