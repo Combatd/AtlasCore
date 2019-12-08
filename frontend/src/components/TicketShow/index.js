46c4096943891ab3cec1de446a9414b8372af82b
Combatd <combatdstar@gmail.com>
2019-12-07 18:13:55 -0800

    getTicket method created on TicketShow component

---
+++ frontend/src/components/TicketShow/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import AddModal from '../layout/AddModal';
+
+
+
+class TicketShow extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+
+    getTicket = async () => {
+        const [params] = this.props.match.params;
+        const ticketId = params.id;
+
+        try {
+            const ticket = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/tickets/${ticketId}`, {
+                credentials: 'include',
+                method: 'Get',
+                'Content-Type': 'application/json'
+            });
+            const ticketJson = await ticket.json();
+            console.log(ticketJson);
+            this.setState({
+                ticket: ticketJson.data.ticket,
+                comments: ticketJson.data.comments
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    
+
+}
+
+export default TicketShow;
