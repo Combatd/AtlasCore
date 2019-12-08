caa9a321c8881f0b86a6514a5c2e87b07add5c59
Combatd <combatdstar@gmail.com>
2019-12-07 20:00:09 -0800

    add componentDidMount() on AddCommentForm

--- frontend/src/components/layout/AddCommentForm/index.js
+++ frontend/src/components/layout/AddCommentForm/index.js
@@ -12,10 +12,40 @@ class AddCommentForm extends Component {
         }
     }
 
+    componentDidMount() {
+        M.AutoInit();
+    }
+
     handleChange = (e) => {
         this.setState({
             [e.currentTarget.name]: e.currentTarget.value
         });
     }
 
+    render() {
+        return(
+            <React.Fragment>
+                <div class="row">
+                    <form class="col s12">
+                        <div class="row">
+                        <div class="input-field col s6">
+                            <input id="input_text" type="text" data-length="10" />
+                            <label for="input_text">Input text</label>
+                        </div>
+                        </div>
+                        <div class="row">
+                        <div class="input-field col s12">
+                            <textarea id="textarea2" class="materialize-textarea" data-length="120" />
+                            <label for="textarea2">Textarea</label>
+                        </div>
+                        </div>
+                    </form>
+                </div>
+ 
+            </React.Fragment>
+        )
+    }
+
 }
+
+export default AddCommentForm;
