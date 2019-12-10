7ae74a068fb45aa8ac13d85cac319c0cb1503780
Combatd <combatdstar@gmail.com>
2019-12-10 02:51:30 -0800

    create User - REGISTER (POST) route

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs');
 const User = require('../models/Users')
 
 
@@ -15,13 +16,34 @@ router.get('/', (req, res) => {
     res.json( { message: "You hit the users GET route!"} )
 });
 
-router.post('/', async (req, res) => {
-    try {
-        const createdUser = await User.create(req.body)
-        res.json(createdUser)
-    } catch (error) {
-        console.log(error)
-    }
+router.post('/register', (req, res) => {
+    
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                // Throw a 400 error if the email address already exists
+                return res.status(400).json({ email: "A user has already registered with this address" })
+            } else {
+                // Otherwise create a new user
+                const newUser = new User({
+                    fullName: req.body.fullName,
+                    email: req.body.email,
+                    password: req.body.password,
+                    isAdmin: req.body.isAdmin
+                })
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    })
+                })
+            }
+        })
+        
 });
 
 router.put('/:userId', async (req, res) => {
