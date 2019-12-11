549f62e1cfd7c935138d087cb7ff3ac46d158b25
Combatd <combatdstar@gmail.com>
2019-12-11 12:56:35 -0800

    create HomePage component boilerplate

---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+class HomePage extends Component {
+    constructor() {
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+        
+    }
+
+    render() {
+        return(
+            <React.Fragment>
+
+
+
+            </React.Fragment>
+        )
+    }
+}
