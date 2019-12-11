bafa6e6c9764692f8f18c3e423ea4fa093df7746
Combatd <combatdstar@gmail.com>
2019-12-10 16:59:18 -0800

    comments can create but not pushing into Ticket

--- frontend/src/components/layout/AddCommentForm/index.js
+++ frontend/src/components/layout/AddCommentForm/index.js
@@ -8,7 +8,8 @@ class AddCommentForm extends Component {
 
         this.state = {
             title: '',
-            text:''
+            text: '',
+            ticketId: ''
         }
     }
 
@@ -18,15 +19,27 @@ class AddCommentForm extends Component {
         });
     }
 
+    addComment = async (e) => {
+        e.preventDefault()
+        const createdComment = await fetch(`/api/v1/tickets/${this.props.ticketId}/comments`, {
+            method: "POST",
+            body: JSON.stringify(this.state),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+    }
+
     render() {
         return(
             <React.Fragment>
                 <div class="row">
-                    <form onSubmit={(e) => this.props.closeAndAdd(e, this.state)} >
+                    <form onSubmit={(e) => this.addComment(e)} >
                         <label>Comment Title</label>
                         <input type="text" name="title" value={this.state.title} placeholder="Comment Title" onChange={this.handleChange} />
                         <label>Text: </label>
-                        <input type="textarea" name="text" value={this.state.text} placeholder="Lorem Ipsum Bacon Ipsum Ham Ipsum Grass Fed Ipsum" onChange={this.handleChange} />
+                        <input type="text" name="text" value={this.state.text} placeholder="Lorem Ipsum Bacon Ipsum Ham Ipsum Grass Fed Ipsum" onChange={this.handleChange} />
+                        <input type="text" name="ticketId" value={this.props.ticketId} hidden />
 
                         <footer>
                             <input type="submit" value="Submit" />
